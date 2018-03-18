62254092ae2ee319ba49b887afc3a3d93365867c
kamalrajchellappa <37336324+kamalrajchellappa@users.noreply.github.com>
2018-03-18 20:45:10 +0530

    Update largest of three number.py

--- beginner/largest of three number.py
+++ beginner/largest of three number.py
@@ -2,9 +2,15 @@ n1=input('Enter any number1:')
 n2=input('Enter any number2:')
 n3=input('Enter any number3:')
 
-if(n1>n2) and (n1>n2):
+if(n1>n2) and (n1>n3):
   print('number1 is greater')
 elif(n2>n1) and (n2>n3):  
   print('number2 nis greater')
+elif(n1==n2) and (n2==n1):
+  print('number1 and number2 are equal')
+elif(n1==n3) and (n3==n1):
+  print('number1 and number3 are equal')
+elif(n2==n3) and (n3==n2):
+  print('number2 and number3 are equal')
 else:
   print('number3 is greater')
